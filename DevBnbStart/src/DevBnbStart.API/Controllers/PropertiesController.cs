a1024ac53a7575e22193a6e0f6adc90b20a0071d
rubensousa-dev <rubenpereira.dev@gmail.com>
2025-08-22 15:31:09 +0100

    Create Project, creat User and Properties Controller

---
+++ DevBnbStart/src/DevBnbStart.API/Controllers/PropertiesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevBnbStart.API.Controllers
+{
+    [ApiController]
+    [Route("api/properties")]
+    public class PropertiesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete ("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ DevBnbStart/src/DevBnbStart.API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevBnbStart.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id)
+        {
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
